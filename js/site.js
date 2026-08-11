fc623f5c9c50fbd55f155d029b737b27ed05c045
ceribas-wx16 <ceribasbso6716@gmail.com>
2026-08-11 12:18:53 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL = "https://lhltolrtgnfkbwfkpaex.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_xdWMVRunvPSeiMw2vfGWyw_l6dTnBsn";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase bağlantısı hazır.");
+console.log("supabaseClient:", typeof supabaseClient);
